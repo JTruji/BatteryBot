2cd2a1b4c0a6b49c471cbae85fe0d636d854a24c
jtruj <jtrujilloan@gmail.com>
2022-03-04 11:50:40 +0100

    adding config and create database tables

--- build.sbt
+++ build.sbt
@@ -2,6 +2,8 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.8"
 
+libraryDependencies += "com.github.pureconfig" %% "pureconfig" % "0.17.1"
+
 lazy val root = (project in file("."))
   .settings(
     name := "BatteryBot"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
+import config.Config
+import pureconfig._
+import pureconfig.generic.auto._
+
+ConfigSource.default.load[ServiceConf]
+// res4: ConfigReader.Result[ServiceConf] = Right(
+//   ServiceConf(
+//     "example.com",
+//     Port(8080),
+//     true,
+//     List(PrivateKey(/home/user/myauthkey), Login("pureconfig", "12345678"))
+//   )
+// )
 
 object Main {
   def main(args: Array[String]): Unit = {
