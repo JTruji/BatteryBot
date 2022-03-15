28ec2362650bb4ce6c19fc2747bde7b9636cf95f
jtruj <jtrujilloan@gmail.com>
2022-03-15 20:21:16 +0100

    First database migration using Flyway

--- src/main/resources/db/migration/V1__first_db_table_creation.sql
+++ src/main/resources/db/migration/V1__first_db_table_creation.sql
@@ -4,10 +4,10 @@ name text not null,
 sleeping_time int,
 wakeup_time int,
 night_charge boolean not null
-)
+);
 
 create table devices(
 id_device uuid primary key,
 name text not null,
 charging_time decimal (2,1) not null
-)
+);
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,20 @@
 import config.Config
+import org.flywaydb.core.Flyway
 import pureconfig._
 import pureconfig.generic.auto._
 
 object Main {
 
-//  Flyway
-//    .configure()
-//    .dataSource(jdbcUrl, dbUserName, dbPassword)
-//    .load()
-//    .migrate
   def main(args: Array[String]): Unit = {
 
-  val config = ConfigSource.default.loadOrThrow[Config]
+    val config = ConfigSource.default.loadOrThrow[Config]
   println (config)
+println(config.database)
+    Flyway
+      .configure()
+      .dataSource(config.database.url, config.database.user, config.database.password)
+      .load()
+      .migrate
 
   }
 }
