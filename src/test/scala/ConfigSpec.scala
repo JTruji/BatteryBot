2e3e7de733acc89f831d2db64b26167a4503b24c
jtruj <jtrujilloan@gmail.com>
2022-03-24 19:18:57 +0100

    Testing pureconfig

--- src/test/scala/ConfigSpec.scala
+++ src/test/scala/ConfigSpec.scala
@@ -6,7 +6,7 @@ import pureconfig.generic.auto._
 
 class ConfigSpec extends CatsEffectSuite{
   test(name ="Config can be decoded from application.conf "){
-    IO(ConfigSource.default.loadOrThrow[Config]).attempt.map(_.isRight).assert
+    IO(ConfigSource.default.load[Config]).map(res => assert(res.isRight, res))
   }
 }
 
