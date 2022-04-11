66eea92fee521d3906cbfc249763e432f9067abc
JTruji <jtrujilloan@gmail.com>
2022-04-11 15:29:11 +0200

    queries to add data into the database successfully tested

--- src/main/scala/database/Queries.scala
+++ src/main/scala/database/Queries.scala
@@ -7,16 +7,16 @@ import java.util.UUID
 
 object Queries {
 
-  def insertUsers(idUsers: UUID, name: String, sleepingTime: Int, wakeupTime: Int, nightCharge: Boolean) =
-    sql"""insert into users (idUsers, name, sleepingTime, wakeupTime, nightCharge)
-    values ($idUsers, $name, $sleepingTime, $wakeupTime, $nightCharge)""".update
+  def insertUsers(id_users: UUID, name: String, sleeping_time: Int, wakeup_time: Int, night_charge: Boolean) =
+    sql"""insert into users (id_users, name, sleeping_time, wakeup_time, night_charge)
+    values ($id_users, $name, $sleeping_time, $wakeup_time, $night_charge)""".update
 
-  def insertDevices(idDevice: UUID, name: String, chargingTime: Float) =
-    sql"""insert into devices (idDevice, name, chargingTime)
-    values ($idDevice, $name, $chargingTime)""".update
+  def insertDevices(id_device: UUID, name: String, charging_time: Double) =
+    sql"""insert into devices (id_device, name, charging_time)
+    values ($id_device, $name, $charging_time)""".update
 
-  def insertPrices(timeRange: Instant, price: BigDecimal) =
+  def insertPrices(time_range: Instant, price: BigDecimal) =
     sql"""insert into prices (time_range, price)
-    values ($timeRange, $price)""".update
+    values ($time_range, $price)""".update
 
 }
--- src/test/scala/database/Specs2Spec.scala
+++ src/test/scala/database/Specs2Spec.scala
@@ -1,17 +1,19 @@
+package database
+
 import doobie.implicits._
 import munit.CatsEffectSuite
 
 class Specs2Spec extends CatsEffectSuite {
 
   case class prices(hour: Int, price: BigDecimal)
-
-  test(name = "data displayed")
-  def takesLongerThan(limitHour: Int) =
-    sql"select hour, price from devices where hour > $limitHour".query[prices]
-
-  test(name = "data can be updated")
-  def updatePrices(newPrice: BigDecimal, hour: Int) =
-    sql"update devices set price = $newPrice where hour = $hour".update
+//
+//  test(name = "data displayed")
+//  def takesLongerThan(limitHour: Int) =
+//    sql"select hour, price from devices where hour > $limitHour".query[prices]
+//
+//  test(name = "data can be updated")
+//  def updatePrices(newPrice: BigDecimal, hour: Int) =
+//    sql"update devices set price = $newPrice where hour = $hour".update
 }
 
 //       val trivial =
