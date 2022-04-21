eabe1ce57fa148f497a794cecd86734bd7006d9d
jtruj <jtrujilloan@gmail.com>
2022-04-21 15:27:54 +0200

    update queries added and testing

--- src/main/scala/database/Queries.scala
+++ src/main/scala/database/Queries.scala
@@ -6,7 +6,7 @@ import java.time.Instant
 import java.util.UUID
 
 object Queries {
-
+  // Insert new data
   def insertUsers(id_users: UUID, name: String, sleeping_time: Int, wakeup_time: Int, night_charge: Boolean) =
     sql"""insert into users (id_users, name, sleeping_time, wakeup_time, night_charge)
     values ($id_users, $name, $sleeping_time, $wakeup_time, $night_charge)""".update
@@ -19,4 +19,17 @@ object Queries {
     sql"""insert into prices (time_range, price)
     values ($time_range, $price)""".update
 
+  // Update user data
+  def updateSleepingTime(name: String, sleeping_time: Int) =
+    sql"""update users set sleeping_time = $sleeping_time where name = $name""".update
+
+  def updateWakeUpTime(name: String, wakeup_time: Int) =
+    sql"""update users set wakeup_time = $wakeup_time where name = $name""".update
+
+  def updateNightCharge(name: String, night_charge: Boolean) =
+    sql"""update users set night_charge = $night_charge where name = $name""".update
+
+  // Update devices data
+  def updateChargingTime(name: String, charging_time: Double) =
+    sql"""update devices set charging_time = $charging_time where name = $name""".update
 }
--- src/test/scala/database/QueriesSuite.scala
+++ src/test/scala/database/QueriesSuite.scala
@@ -10,7 +10,11 @@ class QueriesSuite extends PersistenceBaseSuite with IOChecker {
   test("Queries works as expected") {
     check(Queries.insertPrices(Instant.now(), 0.324));
     check(Queries.insertDevices(UUID.randomUUID(), "patinete", 8.0d));
-    check(Queries.insertUsers(UUID.randomUUID(), "user01", 22, 9, false))
+    check(Queries.insertUsers(UUID.randomUUID(), "user01", 22, 9, false));
+    check(Queries.updateSleepingTime("user01",23));
+    check(Queries.updateWakeUpTime("user01",10));
+    check(Queries.updateNightCharge("user01",true));
+    check(Queries.updateChargingTime("patinete",9.0d))
   }
 
 }
