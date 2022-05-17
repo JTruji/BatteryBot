4535977adc6b77418fc995a1a5f0e5286338ad2e
jtruj <jtrujilloan@gmail.com>
2022-05-17 13:04:47 +0200

    method created

--- src/main/scala/webscraping/WebScraper.scala
+++ src/main/scala/webscraping/WebScraper.scala
@@ -6,16 +6,25 @@ import org.jsoup.nodes.Document
 import java.time.LocalDate
 
   object JsoupHtmlToPlainTextTest extends App {
-
-    val doc: Document = Jsoup.connect("https://tarifaluzhora.es").get
-    val s: String = doc.body.text()
-
-    val date: String = LocalDate.now().toString
-
-    val time= doc.getElementsByAttributeValue("itemprop", "description").text.split(":").toList.map(_.trim).mkString("\n")
-    val price = doc.getElementsByAttributeValue("itemprop", "price").text.split("€/kWh").toList.map(_.trim).mkString("\n")
-
-    println(date)
-    println(time)
-    println(price)
+    def scraperTime = {
+  val doc: Document = Jsoup.connect("https://tarifaluzhora.es").get
+  val s: String = doc.body.text()
+  doc
+    .getElementsByAttributeValue("itemprop", "description")
+    .text
+    .split(":")
+    .toList
+    .map(_.trim)
+    .mkString("\n")
+}
+    def scraperPrice = {
+      val doc: Document = Jsoup.connect("https://tarifaluzhora.es").get
+      val s: String = doc.body.text()
+      doc.getElementsByAttributeValue("itemprop", "price")
+        .text
+        .split("€/kWh")
+        .toList
+        .map(_.trim)
+        .mkString("\n")
+    }
   }
