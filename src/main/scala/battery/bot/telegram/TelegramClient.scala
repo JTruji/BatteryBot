5203dbc0aba107e05d26030101a2926e7b01ddde
jtruj <jtrujilloan@gmail.com>
2022-05-19 21:07:09 +0200

    interpreter HTTP telegram messages

--- src/main/scala/battery/bot/telegram/TelegramClient.scala
+++ src/main/scala/battery/bot/telegram/TelegramClient.scala
@@ -1,14 +1,15 @@
 package battery.bot.telegram
 
 import battery.bot.telegram.models.{TelegramBotCommand, TelegramJSON, TelegramResult}
-import cats.effect.IO
+import cats.effect.{IO, Resource}
 import io.circe.Json
+import org.http4s.{Method, Request, Response}
 import org.http4s.circe.CirceEntityCodec._
 import org.http4s.client.Client
 import org.http4s.implicits._
 
 class TelegramClient(client: Client[IO], telegramToken: String) {
-val telegramUri = uri"https://api.telegram.org"
+  val telegramUri = uri"https://api.telegram.org"
 
   def telegramGetMe: IO[TelegramJSON[TelegramResult]] = {
     client
@@ -48,5 +49,18 @@ val telegramUri = uri"https://api.telegram.org"
         telegramUri / s"bot$telegramToken" / "SetMyCommands"
       )
       .flatTap(hj => IO(println(hj)))
+  }
+
+  def sendMessage(chatId: Long, text: String): IO[Boolean] = {
+    client
+      .successful(
+        Request[IO](
+          method = Method.POST,
+          (telegramUri / s"bot$telegramToken" / "sendMessage")
+            .withQueryParam("chat_id", chatId.toString)
+            .withQueryParam("text", text)
+        )
+      )
+      .flatTap(hj => IO(println(s"sendMessage: $hj")))
   }
 }
