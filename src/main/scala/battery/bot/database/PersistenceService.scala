d9c69ce8c659b5b2b1d334d61876250b4b4e0821
JTruji <jtrujilloan@gmail.com>
2022-05-23 11:33:17 +0200

    format and comment solved

--- src/main/scala/battery/bot/database/PersistenceService.scala
+++ src/main/scala/battery/bot/database/PersistenceService.scala
@@ -7,21 +7,16 @@ import battery.bot.database.UsersQueries.insertUsers
 import doobie.Transactor
 import doobie.implicits._
 
-import java.util.{Random, UUID}
-import java.time.Instant
+import java.util.UUID
 
 class PersistenceService(ta: Transactor[IO]) {
 
-  def addPrice(hour: Int, price: BigDecimal): IO[Int] ={
-    insertPrices(Instant.now(), price).run.transact(ta)
-  }
+  def addPrice(hour: Int, price: BigDecimal): IO[Int] =
+    insertPrices(hour, price).run.transact(ta)
 
-  def addUser(name:String, sleepingTime: Int, wakeUpTime:Int, nightCharge:Boolean): IO[Int] = {
+  def addUser(name: String, sleepingTime: Int, wakeUpTime: Int, nightCharge: Boolean): IO[Int] =
     insertUsers(UUID.randomUUID(), name, sleepingTime, wakeUpTime, nightCharge).run.transact(ta)
-  }
 
-  def addDevice(name: String, chargingTime:Double): IO[Int] ={
+  def addDevice(name: String, chargingTime: Double): IO[Int] =
     insertDevices(UUID.randomUUID(), name, chargingTime).run.transact(ta)
-  }
 }
-
