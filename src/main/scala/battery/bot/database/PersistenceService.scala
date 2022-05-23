ad2ca32fab41d1ef8088a0c6346aa4a351227a1b
JTruji <jtrujilloan@gmail.com>
2022-05-23 11:10:45 +0200

    new queries

--- src/main/scala/battery/bot/database/PersistenceService.scala
+++ src/main/scala/battery/bot/database/PersistenceService.scala
@@ -1,22 +1,27 @@
 package battery.bot.database
 
+import battery.bot.database.DevicesQueries.insertDevices
 import cats.effect.IO
 import battery.bot.database.PricesQueries.insertPrices
+import battery.bot.database.UsersQueries.insertUsers
 import doobie.Transactor
 import doobie.implicits._
 
+import java.util.{Random, UUID}
 import java.time.Instant
 
 class PersistenceService(ta: Transactor[IO]) {
 
-  def addPrice(hour: Int, price: BigDecimal) ={
+  def addPrice(hour: Int, price: BigDecimal): IO[Int] ={
     insertPrices(Instant.now(), price).run.transact(ta)
   }
-  //insertUsers().run.transact(ta)
 
-  //insertDevices().run.transact(ta)
+  def addUser(name:String, sleepingTime: Int, wakeUpTime:Int, nightCharge:Boolean): IO[Int] = {
+    insertUsers(UUID.randomUUID(), name, sleepingTime, wakeUpTime, nightCharge).run.transact(ta)
+  }
 
-  //insertPrices(, 0.123).run.transact(ta)
-
+  def addDevice(name: String, chargingTime:Double): IO[Int] ={
+    insertDevices(UUID.randomUUID(), name, chargingTime).run.transact(ta)
+  }
 }
 
