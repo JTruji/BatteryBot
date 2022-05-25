92e586e28dbf66b6f4dc4aea217473df08127470
jtruj <jtrujilloan@gmail.com>
2022-05-25 10:51:03 +0200

    old PricesQueries deleted

--- src/main/scala/battery/bot/database/PersistenceService.scala
+++ src/main/scala/battery/bot/database/PersistenceService.scala
@@ -8,16 +8,9 @@ import java.time.Instant
 
 class PersistenceService(ta: Transactor[IO]) {
 
-  def addPrice(hour: Int, price: BigDecimal): IO[Int] ={
-    PricesQueries.insertPrices(Instant.now(), price).run.transact(ta)
-  }
   def addScraperPrices(pricesList:List[(Instant,BigDecimal)]): IO[Int] ={
       PricesQueries.insertManyPrices.updateMany(pricesList).transact(ta)
   }
-
-  //insertDevices().run.transact(ta)
-
-  //insertPrices(, 0.123).run.transact(ta)
 
 }
 
--- src/main/scala/battery/bot/database/PricesQueries.scala
+++ src/main/scala/battery/bot/database/PricesQueries.scala
@@ -8,10 +8,7 @@ import java.math.BigDecimal
 import java.time.Instant
 
 object PricesQueries {
-  def insertPrices(timeRange: Instant, price: BigDecimal): doobie.Update0 =
-    sql"""insert into prices (time_range, price) values ($timeRange, $price)""".update
-
-  val insertManyPrices: Update[(Instant, BigDecimal)] =
+   val insertManyPrices: Update[(Instant, BigDecimal)] =
     Update[(Instant, BigDecimal)]("insert into prices (time_range, price) values (?, ?)")
 
 }
