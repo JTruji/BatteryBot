33c69869db0f6791a0b5474c277564d0968eedc3
jtruj <jtrujilloan@gmail.com>
2022-06-05 09:28:03 +0200

    interpreter changed

--- src/main/scala/battery/bot/core/CommandProcess.scala
+++ src/main/scala/battery/bot/core/CommandProcess.scala
@@ -7,9 +7,9 @@ import cats.implicits.toTraverseOps
 class CommandProcess {
 
   def interpreter (results: List[TelegramResult], telegramClient: TelegramClient) = {
-    val tbd = results.flatMap(result => result.message.map(message =>(message.chat.id,message.text)))
-    tbd.traverse{
-      case (chatId,"/help") => telegramClient.sendMessage(chatId,"WIP")
+    val jsonResult = results.flatMap(result => result.message.map(message =>(message.chat.id,message.text.split(";").toList.map(_.trim).head)))
+    jsonResult.traverse{
+      case (chatId,"/help") => telegramClient.sendMessage(chatId,s"Entro en el comando")
       case (chatId,message) => telegramClient.sendMessage(chatId,s"No se ha detectado ningún comando: $message")
     }
   }
