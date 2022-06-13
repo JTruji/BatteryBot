14f466d97915ba93329ab0c56f715fe52f499331
JTruji <jtrujilloan@gmail.com>
2022-06-13 16:20:17 +0200

    format

--- src/main/scala/battery/bot/database/DevicesQueries.scala
+++ src/main/scala/battery/bot/database/DevicesQueries.scala
@@ -6,7 +6,7 @@ import doobie.postgres.implicits._
 
 object DevicesQueries {
   def insertDevices(idDevice: UUID, userId: UUID, name: String, chargingTime: Double): doobie.Update0 =
-    sql"""insert into devices (id_device, id_user, name, charging_time) values ($idDevice, $userId, $name, $chargingTime) on conflict do nothing""".update
+    sql"""insert into devices (id_device, id_user, name, charging_time) values ($idDevice, $userId, $name, $chargingTime)""".update
 
   def updateChargingTime(name: String, chargingTime: Double): doobie.Update0 =
     sql"""update devices set charging_time = $chargingTime where name = $name""".update
