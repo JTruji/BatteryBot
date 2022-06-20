ab1b84f25527065ec60be785d3038ae4d180b0b6
jtruj <jtrujilloan@gmail.com>
2022-06-20 19:22:57 +0200

    format

---
+++ src/main/scala/battery/bot/streaming/StreamingProcess.scala
@@ -0,0 +1,36 @@
+package battery.bot
+import battery.bot.core.CommandProcess
+import battery.bot.telegram.TelegramClient
+import battery.bot.telegram.models.TelegramUpdate
+import cats.effect.{ExitCode, IO}
+import fs2.Stream
+import org.log4s.MDC.result
+
+import scala.concurrent.duration.DurationInt
+class StreamingProcess(telegramClient: TelegramClient, commandProcess: CommandProcess) {
+
+  def process[TelegramClient]: Stream[IO, Unit] =
+    streamProcess[List[TelegramUpdate]](lastUpdateId =>
+      telegramClient.telegramGetUpdate.map { result =>
+        //handle empty list
+        val newLastUpdateId =
+          result.result.map(_.updateId).maxOption.getOrElse(lastUpdateId)
+        val filteredUpdates = result.result.filter(_.updateId > lastUpdateId)
+        (newLastUpdateId, filteredUpdates)
+      }
+    )
+      .flatMap(result => Stream.emits(result))
+      .evalMap(update =>
+  for {
+    lastJSON <- commandProcess.interpreter(update)
+  }yield (lastJSON))
+
+  def streamProcess[A](getUpdates: Long => IO[(Long, A)]): Stream[IO, A] =
+    Stream
+      .awakeDelay[IO](2.seconds)
+      .evalMapAccumulate(0L) { (startPoint, _) =>
+        getUpdates(startPoint)
+      }
+      .map(_._2)
+
+}
