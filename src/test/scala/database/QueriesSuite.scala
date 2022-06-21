d9ad3366ace7296a756ea207b5d47126d3005d93
jtruj <jtrujilloan@gmail.com>
2022-06-22 00:25:08 +0200

    testing corrected

--- src/test/scala/database/QueriesSuite.scala
+++ src/test/scala/database/QueriesSuite.scala
@@ -9,18 +9,18 @@ import java.util.UUID
 class QueriesSuite extends PersistenceBaseSuite with IOChecker {
 
   test("Price queries works as expected") {
-//    check(PricesQueries.insertManyPrices)
+    check(PricesQueries.insertManyPrices)
   }
   test("Devices queries works as expected") {
-//    check(DevicesQueries.insertDevices(UUID.randomUUID(), UUID.randomUUID(), "patinete", 8.0d));
-//    check(DevicesQueries.updateChargingTime("patinete", 9.0d, UUID.randomUUID()))
-//    check(DevicesQueries.userDevicesName(UUID.randomUUID()))
-//    check(DevicesQueries.deleteDevice(UUID.randomUUID(),"patinete"))
+    check(DevicesQueries.insertDevices(UUID.randomUUID(), UUID.randomUUID(), "patinete", 8.0d));
+    check(DevicesQueries.updateChargingTime("patinete", 9.0d, UUID.randomUUID()))
+    check(DevicesQueries.userDevicesName(UUID.randomUUID()))
+    check(DevicesQueries.deleteDevice(UUID.randomUUID(),"patinete"))
   }
   test("Users queries works as expected") {
     check(UsersQueries.insertUsers(UUID.randomUUID(), 1L, 22, 9, false))
-//    check(UsersQueries.updateSettings("user01", 23, 10, true))
-//    check(UsersQueries.getUserUUID("user01"))
-//    check(UsersQueries.getSettings("user01"))
+    check(UsersQueries.updateSettings(UUID.randomUUID(), 23, 10, true))
+    check(UsersQueries.getUserId(1L))
+    check(UsersQueries.getSettings(UUID.randomUUID()))
   }
 }
